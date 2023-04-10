e50646d128267c62570b79ac3f2ef3d6e29982a2
Seb <sebheron21@hotmail.co.uk>
2023-04-11 00:19:15 +0100

    modify active setting

--- js/mobile-display.js
+++ js/mobile-display.js
@@ -1,13 +1,16 @@
 const backgrounds = document.querySelectorAll('.background');
 
+setActive = (background, active) => {
+  if (active) {
+    background.classList.add('active');
+  } else {
+    background.classList.remove('active');
+  }
+};
+
 window.addEventListener('scroll', () => {
+  if (window.innerWidth > 768) return;
   backgrounds.forEach((background) => {
-    //If the background is within 200px of the center of the screen, add the class 'active'
-    if (background.getBoundingClientRect().top < window.innerHeight / 3
-      && background.getBoundingClientRect().bottom > window.innerHeight / 3) {
-      background.classList.add('active');
-    } else {
-      background.classList.remove('active');
-    }
+    setActive(background, background.getBoundingClientRect().top < window.innerHeight / 3 && background.getBoundingClientRect().bottom > window.innerHeight / 3);
   });
 });
