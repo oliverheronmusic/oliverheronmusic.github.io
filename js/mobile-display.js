ad07e0d0e1926eaaf0560b0c669bb20159b4fb5f
Seb <sebheron21@hotmail.co.uk>
2023-04-11 02:14:13 +0100

    bottom menu

--- js/mobile-display.js
+++ js/mobile-display.js
@@ -1,4 +1,5 @@
 const boxes = document.querySelectorAll('.box');
+const icons = document.querySelectorAll('.icons-row');
 
 setActive = (box, active) => {
   if (active) {
@@ -8,9 +9,13 @@ setActive = (box, active) => {
   }
 };
 
-window.addEventListener('scroll', () => {
+scrolled = () => {
   if (window.innerWidth > 768) return;
   boxes.forEach((box) => {
-    setActive(box, box.getBoundingClientRect().top < window.innerHeight / 3 && box.getBoundingClientRect().bottom > window.innerHeight / 3);
+    setActive(box, box.getBoundingClientRect().top < window.innerHeight / 3 + 100 && box.getBoundingClientRect().bottom > window.innerHeight / 3 + 50);
   });
-});
+  setActive(icons[0], window.innerHeight + window.scrollY >= document.body.offsetHeight - 10);
+}
+
+window.addEventListener('scroll', scrolled);
+scrolled();
